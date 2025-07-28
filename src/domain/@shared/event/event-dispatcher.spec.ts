8deb2dd0a8471081ecb63915a5ba52d0bff4cbd5
lucaskevencosta <lucaskevencosta@gmail.com>
2025-07-28 12:04:55 -0300

    Second challenge solving - Course Full Cycle DDD: Modelagem Tática e Patterns

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,3 +1,8 @@
+import CustomerAddressChangedEvent from "../../customer/event/customer-address-changed.event";
+import CustomerCreatedEvent from "../../customer/event/customer-created.event";
+import WriteConsoleLogWhenAddressIsChangedHandler from "../../customer/event/handler/write-console-log-when-address-is-changed.handler";
+import WriteFirstConsoleLogWhenCustomerIsCreatedHandler from "../../customer/event/handler/write-first-console-log-when-customer-is-created.handler";
+import WriteSecondConsoleLogWhenCustomerIsCreatedHandler from "../../customer/event/handler/write-second-console-log-when-customer-is-created.handler";
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -76,6 +81,65 @@ describe("Domain events tests", () => {
 
     // Quando o notify for executado o SendEmailWhenProductIsCreatedHandler.handle() deve ser chamado
     eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+  it("should notify all custumer created event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const firstEventHandler = new WriteFirstConsoleLogWhenCustomerIsCreatedHandler();
+    const secondEventHandler = new WriteSecondConsoleLogWhenCustomerIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(firstEventHandler, "handle");
+    const spyEventHandler2 = jest.spyOn(secondEventHandler, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", firstEventHandler);
+    eventDispatcher.register("CustomerCreatedEvent", secondEventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(firstEventHandler);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(secondEventHandler);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      id: "customerId1",
+      name: "customerName",
+      address: {
+        street: "Address strees",
+        number: 0
+      },
+      active: true,
+      rewardPoints: 50
+    });
+
+    // // Quando o notify for executado o WriteFirstConsoleLogWhenCustumerIsCreatedHandler.handle() e o 
+    // // WriteSecondConsoleLogWhenCustumerIsCreatedHandler.handle()  devem ser chamados
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+  });
+
+  it("should notify address changed event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new WriteConsoleLogWhenAddressIsChangedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]).toMatchObject(eventHandler);
+
+    const customerAddressChangedEvent = new CustomerAddressChangedEvent({
+      id: "customerId1",
+      name: "customerName",
+      address: {
+        street: "Address strees",
+        number: 0,
+        zip: 12312-123,
+        city: "Address city"
+      }
+    });
+    
+    // Quando o notify for executado o WriteFirstConsoleLogWhenCustumerIsCreatedHandler.handle() e o 
+    // WriteSecondConsoleLogWhenCustumerIsCreatedHandler.handle()  devem ser chamados
+    eventDispatcher.notify(customerAddressChangedEvent);
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
---
+++ src/domain/customer/event/handler/write-console-log-when-address-is-changed.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerAddressChangedEvent from "../customer-address-changed.event";
+
+export default class WriteConsoleLogWhenAddressIsChangedHandler 
+  implements EventHandlerInterface<CustomerAddressChangedEvent> 
+{
+  handle(event: CustomerAddressChangedEvent): void {
+    console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.nome} alterado para: ${event.eventData.address.toString()}`);
+  }
+}
